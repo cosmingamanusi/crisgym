454fbbc600764155e379765a1cab6589a45536a6
cosmin-gamanusi <cosmin.gamanusi@gmail.com>
2020-08-15 18:07:06 +0300

    finished homepage structure

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -5,7 +5,7 @@ window.addEventListener("scroll", () => {
     if (scroll > 0) {
         navBar.style.backgroundColor = "black"
     } else {
-        navBar.style.backgroundColor = "rgba(0,0,0,0.7)"
+        navBar.style.backgroundColor = "rgba(0,0,0,0.5)"
         navBar.style.transitionDuration = "0.5s"
     }
 })
